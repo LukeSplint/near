fae60b622917d4d3fe59fc780bc63e2671ebdd4f
Lukasz Drzyzga <lukedrzyzga@gmail.com>
2016-02-11 23:22:12 +0100

    add package

--- src/main/java/com/nearsight/SimpleRestController.java
+++ src/main/java/com/nearsight/SimpleRestController.java
@@ -1,3 +1,5 @@
+package com.nearsight;
+
 import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
